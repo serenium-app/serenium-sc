bdeb56c087ecafc9cc57cbd24b02a4b25696af2a
jonar12 <jona.arredondo12@gmail.com>
2024-02-28 20:21:41 -0600

    ISSUE 296: Done with setting up the state crate for the Storage contract

---
+++ storage/state/src/lib.rs
@@ -0,0 +1,39 @@
+#![no_std]
+
+use gmeta::metawasm;
+use gstd::{prelude::*, ActorId};
+use io::*;
+use storage_io::*;
+
+#[metawasm]
+pub mod metafns {
+    pub type State = IoThreadStorage;
+
+    pub fn reward_data(_state: State) -> Option<()> {
+        None
+    }
+
+    pub fn distributed_tokens(_state: State) -> Option<u64> {
+        None
+    }
+
+    pub fn thread_status(_state: State) -> Option<ThreadStatus> {
+        None
+    }
+
+    pub fn all_threads(_state: State) -> Option<IoThreads> {
+        None
+    }
+
+    pub fn graph_rep(_state: State) -> Option<Vec<(PostId, Vec<PostId>)>> {
+        None
+    }
+
+    pub fn like_history(_state: State) -> Option<Vec<(ActorId, u64)>> {
+        None
+    }
+
+    pub fn thread_by_post_id(_state: State, _target_post_id: PostId) -> Option<IoThread> {
+        None
+    }
+}
